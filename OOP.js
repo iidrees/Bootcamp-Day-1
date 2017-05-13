2a164af578e56466172130662f8a40c7bfe33a25
Idrees Ibraheem <idreesibraheem@gmail.com>
2017-05-13 19:30:49 +0100

    Fix bugs

--- OOP.js
+++ OOP.js
@@ -1,65 +1,65 @@
 class GTBankAccount {
-		constructor(holderName, accNumber, address,minimumBalance, currentBalance) {
-			this._holderName = holderName;
-			this._accNumber = accNumber;
-			this._address = address;
-			this._minimumBalance = minimumBalance;
-			this._currentBalance = currentBalance;
-			
-		}
-	    get fullAccountDetails() {
-		     let details = {
-		       name: this._holderName,
-		       accNumber: this._accNumber,
-		       address: this._address,
-		       minBal: this._minimumBalance,
-		       currBal: this._currentBalance
-		     };
-		     return details;
-		  }
-      get minBalanceDetails() {
+	constructor(holderName, accNumber, address,minimumBalance, currentBalance) {
+		this._holderName = holderName;
+		this._accNumber = accNumber;
+		this._address = address;
+		this._minimumBalance = minimumBalance;
+		this._currentBalance = currentBalance;
+		
+	}
+    get fullAccountDetails() {
+	    let details = {
+	    	name: this._holderName,
+	    	accNumber: this._accNumber,
+	    	address: this._address,
+	    	minBal: this._minimumBalance,
+	    	currBal: this._currentBalance
+	     };
+	     return details;
+	  }
+    get minBalanceDetails() {
         return this._currentBalance;
-      }
-      
-      
-      accDeposit(depositAmt) {
+    }
+  
+  
+    accDeposit(depositAmt) {
         return 'Thank you for banking with us, your current Balance now is ' + (this._currentBalance +=  depositAmt);
 		
 	}
 
 
-      accWithdrawal(amount) {
-    	  if (amount > this._currentBalance) {
-    			return 'Please your account balance is insufficient';
-    		} else {
-    			return 'Thank you for banking with us, your current Balance now is ' + (this._currentBalance -=  amount);
-    		}
-    	}
+	accWithdrawal(amount) {
+		if (amount > this._currentBalance) {
+			return 'Please your account balance is insufficient';
+		} else {
+			return 'Thank you for banking with us, your current Balance now is ' + (this._currentBalance -=  amount);
+		}
 	}
+}
 
 class GTSavingsAccount extends GTBankAccount {
-		constructor(holderName, accNumber, address,minimumBalance, currentBalance) {
-			super(holderName, accNumber, address,minimumBalance, currentBalance);
-			
-		}
-
-		addAccountHolders(GTBankAccount) {
-		  let savingsAccountHolders = [];
-			savingsAccountHolders.push(GTBankAccount);
-			return savingsAccountHolders;
-		}
+	constructor(holderName, accNumber, address,minimumBalance, currentBalance) {
+		super(holderName, accNumber, address,minimumBalance, currentBalance);
 		
+	}
+
+	addAccountHolders(GTBankAccount) {
+	    let savingsAccountHolders = [];
+		savingsAccountHolders.push(GTBankAccount);
+		return savingsAccountHolders;
+	}
+	
 
 	}
 
 class GTCurrentAccount extends GTBankAccount {
-		constructor (holderName, accNumber, address,minimumBalance, currentBalance) {
-			super(holderName, accNumber, address,minimumBalance, currentBalance);
-			
-		}
+	constructor (holderName, accNumber, address,minimumBalance, currentBalance) {
+		super(holderName, accNumber, address,minimumBalance, currentBalance);
+		
+	}
 
-		addAccountHolders(GTBankAccount) {
-		  let currentAccountHolders = [];
-			this.currentAccountHolders.push(GTBankAccount);
-		}
+	addAccountHolders(GTBankAccount) {
+	    let currentAccountHolders = [];
+	    this.currentAccountHolders.push(GTBankAccount);
+	}
 }
